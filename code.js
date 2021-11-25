cc495a6f0790b9aadc69b285709fd1c4ab53c5b3
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2021-11-25 01:43:52 -0400

    Hiting the ball and telling when is foul

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+'use strinct';
+
 class Pitch {
 	constructor(type, speed) {
 		this.Type = type
@@ -7,6 +9,7 @@ class Pitch {
 }
 
 class Batter {
+
 	constructor(name, sickness, fastball, change, curve, slider, knuckles) {
 		this.Name = name
 		this.Sickness = sickness
@@ -17,26 +20,59 @@ class Batter {
 		this.Knuckles = knuckles
 		this.PitchHistory = []
 	}
+
+	async swing(pitch) {
+		let contactProbability = this.getContactProbability(pitch, this)
+		const willHitTheBall = Math.random() > 1 - (contactProbability / 100)
+
+		if (willHitTheBall) {
+			console.log('hitted');
+			await moveBallToRandomPosition()
+		} else {
+			console.log('---not hitted---');
+		}
+
+		const batterElement = document.querySelector('.batter');
+		batterElement.style.transform = 'rotate(-90deg)'
+		await sleep(400)
+		batterElement.style.transform = 'rotate(0deg)'
+	}
+
+	getContactProbability(pitch, batter) {
+		let probabilityPercentage = batter[pitch.Type]
+		probabilityPercentage -= sickessProbabilityDecrease[batter.Sickness]
+		return probabilityPercentage
+	}
 }
 
 const PitchTypes = {
-	fastball: 'fastball',
-	change: 'change',
-	curve: 'curve',
-	slider: 'slider',
-	knuckles: 'knuckles'
+	fastball: 'Fastball',
+	change: 'Change',
+	curve: 'Curve',
+	slider: 'Slider',
+	knuckles: 'Knuckles'
 }
 
 const SicknessTypes = {
-	fastball: 'fastball',
-	change: 'change',
-	curve: 'curve',
-	slider: 'slider',
-	knuckles: 'knuckles'
+	flu: 'flu',
+	hangover: 'hangover',
+	badNight: 'badNight',
+	none: 'none'
 }
 
 const config = {
+	sickPlayerProbability: 0.40
+}
 
+const sickessProbabilityDecrease = {
+	flu: 5,
+	hangover: 10,
+	badNight: 20,
+	none: 0
+}
+
+const state = {
+	currentBatter : null
 }
 
 const fieldContainer = document.querySelector('.field-container');
@@ -47,14 +83,88 @@ const ball = document.querySelector('.ball');
 main()
 
 function main() {
-	//throwBall *
-	//swing
-
+	setRandomBatter()
+	// setInterval(() => {
+	// 	pitchAndSwing()
+	// }, 2000);
 }
 
-function pitchAndSwing() {
+function setRandomBatter() {
+	state.currentBatter = new Batter(
+		'Player' + randomIntFromInterval(1, 1000),	//name
+		getRandomHealth(), 							//health
+		randomIntFromInterval(70, 100), 			//fastball
+		randomIntFromInterval(60, 100), 			//change
+		randomIntFromInterval(70, 90), 				//curve
+		randomIntFromInterval(60, 90), 				//slider
+		randomIntFromInterval(70, 100) 				//knuckles
+	)
+}
+
+async function moveBallToRandomPosition() {
+	let x = randomIntFromInterval(1, 1000)
+	let y = randomIntFromInterval(1, 1000)
+
+	//todo: try to use the id of the pitch to avoid creating a new div for the ball
+
+	let ball = document.createElement('div')
+	fieldContainer.appendChild(ball)
+	ball.id = crypto.randomUUID()
+	ball.classList.add('obj', 'ball', 'flying')
+	ball.style.top = '718px'
+	ball.style.left = '493px'
+
+	await sleep(50)
+	ball.style.top = `${y}px`
+	ball.style.left = `${x}px`
+
+	let isFoul = isFoulPitch(x, y)
+}
+
+function isFoulPitch(x2, y2) {
+	const y1 = 735
+	const x1 = 500
+
+	if (y2 > y1) {
+		console.log('foul y')
+		return true
+	}
+
+	const angleRad = Math.atan((y2 - y1) / (x2 - x1))
+	const angle = (angleRad * 180 / Math.PI) * -1;
+
+
+	if (angle > -45 && angle < 45) {
+		console.log('foul')
+		return true
+	}
+
+	return false
+}
+
+function getRandomHealth() {
+	let isSick = Math.random() > 1 - config.sickPlayerProbability
+
+	if (!isSick) {
+		return SicknessTypes.none
+	}
+
+	let sicknessNumber = randomIntFromInterval(1, 3)
+
+	if (sicknessNumber == 1) {
+		return SicknessTypes.flu
+
+	} else if (sicknessNumber == 2) {
+		return SicknessTypes.hangover
+
+	} else {
+		return SicknessTypes.badNight
+	}
+}
+
+async function pitchAndSwing() {
 	let pitch = await throwBall()
-	//I was here. hit the ball
+	await state.currentBatter.swing(pitch)
 }
 
 async function throwBall() {
